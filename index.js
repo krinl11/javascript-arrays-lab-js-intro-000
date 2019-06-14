d6b0ebe2fefc49ba9657869ec100d4e0ebc40d2d
Karina Evers <krin_11_bball@yahoo.com>
2019-06-14 21:25:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var kittens = [
 ] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten (array, element){
+function destructivelyAppendKitten (){
   return [...kittens, "Ralph"]
 
 }
